474acbb974ca9f29a696e8ab22a1d88277c8910b
George Paulino <george7paulino@gmail.com>
2022-05-15 08:47:54 -0300

    Renamed Classes from LanMessage.cs

---
+++ Script/Objects/LanMessage.cs
@@ -0,0 +1,23 @@
+using System.Numerics;
+using Lidgren.Network;
+namespace PingPong
+{
+    public class ClientMessage
+    {
+        public bool up;
+        public bool down;
+    }
+    public class ServerMessage
+    {
+        //public Vector2[] paddlePos = new Vector2[2];
+        public float pad1X;
+        public float pad1Y;
+        public float pad2X;
+        public float pad2Y;
+        public float ballX;
+        public float ballY;
+        public int score1;
+        public int score2;
+        public int win;
+    }
+}
